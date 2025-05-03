5cf7570b3a2b49a474be72a2edc4af1e5aa783a1
Jose Alejandro Barrios <jose2212278@correo.uis.edu.co>
2025-05-03 15:14:00 -0500

    feat: run tests

--- distilling_classifier/run_distillation.py
+++ distilling_classifier/run_distillation.py
@@ -8,55 +8,7 @@ p4 = r"C:\Users\SERGIOURREA\Desktop\KD_Jose\distilling_classifier\save_model_t\m
 p5 = r"C:\Users\SERGIOURREA\Desktop\KD_Jose\distilling_classifier\save_model_t\model_binary_20.pth"
 p6 = r"C:\Users\SERGIOURREA\Desktop\KD_Jose\distilling_classifier\save_model_t\model_binary_10.pth"
 
-    
-
-# KLDivLoss
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.4} --SPC_portion_st {0.1} --teacher_path {p4} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.4} --SPC_portion_st {0.1} --teacher_path {p4} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.4} --SPC_portion_st {0.1} --teacher_path {p4} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.4} --SPC_portion_st {0.1} --teacher_path {p4} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.2} --SPC_portion_st {0.1} --teacher_path {p5} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.2} --SPC_portion_st {0.1} --teacher_path {p5} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.2} --SPC_portion_st {0.1} --teacher_path {p5} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.2} --SPC_portion_st {0.1} --teacher_path {p5} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.1} --SPC_portion_st {0.1} --teacher_path {p6} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.1} --SPC_portion_st {0.1} --teacher_path {p6} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.1} --SPC_portion_st {0.1} --teacher_path {p6} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda2 {0} --lambda3 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.1} --SPC_portion_st {0.1} --teacher_path {p6} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
 # Optics Loss
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {1.0} --SPC_portion_st {0.1} --teacher_path {p1} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.8} --SPC_portion_st {0.1} --teacher_path {p2} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.6} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
-os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.8} --milestones 30 50 70 80 --SPC_portion_tchr {0.6} --SPC_portion_st {0.1} --teacher_path {p3} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
 
 os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.2} --milestones 30 50 70 80 --SPC_portion_tchr {0.4} --SPC_portion_st {0.1} --teacher_path {p4} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
 os.system(f"python main_cifar10_KD.py --lambda1 {l1} --lambda3 {0} --lambda2 {0.4} --milestones 30 50 70 80 --SPC_portion_tchr {0.4} --SPC_portion_st {0.1} --teacher_path {p4} --real_st {"False"} --real_tchr {"False"} --project_name {"KD_Losses_B-B"}")
