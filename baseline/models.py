42aee538a0861ccaf00d4a5e39d811db44ac275a
Jose Alejandro Barrios <jose2212278@correo.uis.edu.co>
2025-05-07 21:07:42 -0500

    feat: resnet updates

--- baseline/models.py
+++ baseline/models.py
@@ -15,9 +15,9 @@ class CI_model(nn.Module):
         self.net = ResNet18(num_channels=input_size[0],num_classes=10)
 
     def forward(self, x):
-        x = self.system_layer(x)
-        x = self.net(x)
-        return x    
+        x_hat = self.system_layer(x)
+        x_hat = self.net(x_hat)
+        return x_hat
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -25,17 +25,19 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+        # add dropout layer
+        self.dropout = nn.Dropout(p=0.5)    
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+                               stride=1, padding=1, bias=True)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
+                          kernel_size=1, stride=stride, bias=True),
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
@@ -52,7 +54,9 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+                               stride=1, padding=1, bias=True)
+        # add dropout layer
+        self.dropout = nn.Dropout(p=0.5)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -84,8 +88,8 @@ def ResNet18(num_channels=3, num_classes=10):
     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=num_classes, num_channels=num_channels)
     
 # if __name__ == "__main__":
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # img = torch.randn(32, 3, 32, 32).to(device)
-    # model = CI_model(input_size=(3, 32, 32), snapshots=100, real="False").to(device)
-    # out = model(img)
-    # print(out.size())
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     img = torch.randn(32, 3, 32, 32).to(device)
+#     model = CI_model(input_size=(3, 32, 32), snapshots=100, real="False").to(device)
+#     out = model(img)
+#     print(out.shape)
