97b93d16fa5f6451a6159bd95c85fd8b8bb1362e
Jose Alejandro Barrios <jose2212278@correo.uis.edu.co>
2025-05-08 23:58:18 -0500

    feat: run kl

--- distilling_classifier/MSE_LOSS_ONLY/run_mse_only.py
+++ distilling_classifier/MSE_LOSS_ONLY/run_mse_only.py
@@ -1,19 +1,12 @@
 import os
-os.chdir("distilling_classifier/MSE_LOSS_ONLY")
+# os.chdir("distilling_classifier/MSE_LOSS_ONLY")
 
-lambda2 = [0.25, 0.5, 0.75, 1] 
-lambda1 = 1
-students = [0.05, 0.1]
-teachers = [0.2, 0.4]
+lambda3 = [0.25, 0.5, 0.75, 1] 
 epochs = 50
-for st in students:
-    for t in teachers:
-        for l2 in lambda2:
-            path = r"save_model_t\model_binary_"+str(int(t*100))+".pth"
-            #print(path)
-            if st == 0.05 and t == 0.2:
-                #print("Skip this experiment")
-                pass
-            else:
-                #print(f"Running experiment with SPC_portion_st: {st}, SPC_portion_tchr: {t}, lambda2: {l2}")
-                os.system(f"python main_cifar10_KD_OP.py --num_epochs {epochs} --lambda1 1 --lambda2 {l2} --milestones 20 40 --SPC_portion_tchr {t} --SPC_portion_st {st} --teacher_path {path}")
+temperatura = [2, 4, 6]
+
+for l3 in lambda3:
+    for temp in temperatura:
+        path = r"save_model_t/model_binary_10.pth"
+        # print(f"Running experiment with SPC_portion_st: {0.05}, SPC_portion_tchr: {0.01}, lambda2: {l2}, temp: {temp}")
+        os.system(f"python main_cifar10_KD_N.py --num_epochs {epochs} --lambda1 1 --lambda3 {l3} --milestones 20 40 --SPC_portion_tchr {0.1} --SPC_portion_st {0.01} --teacher_path {path}")
