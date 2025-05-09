889353cad45856a0bcd034840b26f57e581cbc68
Jose Alejandro Barrios <jose2212278@correo.uis.edu.co>
2025-05-08 20:45:51 -0500

    feat: edit runs

--- distilling_classifier/KL_LOSS_ONLY/run_kl_only.py
+++ distilling_classifier/KL_LOSS_ONLY/run_kl_only.py
@@ -1,6 +1,6 @@
 import os
 
-p1 = r"save_model_t\model_binary_100.pth"
-lambda3 = [] 
+p1 = r"save_model_t\model_binary_5.pth"
+lambda3 = [0.25, 0.5, 0.75, 1] 
 for l3 in lambda3:
-    os.system(f"python run_kl_only.py --lambda1 1 --lambda3 {l3} --milestones 20 40 --SPC_portion_tchr 1 --SPC_portion_st 0.1 --teacher_path {p1}")
+    os.system(f"python run_kl_only.py --lambda1 1 --lambda3 {l3} --milestones 20 40 --temperature 6 --SPC_portion_tchr 0.5 --SPC_portion_st 0.01 --teacher_path {p1}")
